03cba0aa8c7ac4f26fca7a87e99785fef424205a
Kate-qp <kpark4328@gmail.com>
2025-05-05 23:58:58 +0300

    Исправление файла

--- utils/random-value.js
+++ utils/random-value.js
@@ -1,6 +1,10 @@
 const getRandomNumber = (min = 0, max = 100) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
-const getRandomIndex = (data) => Math.floor(Math.random() * data.length);
-
+const getRandomIndex = (data) => {
+  if (data.length === 0) {
+    throw new Error('Array is empty');
+  }
+  return Math.floor(Math.random() * data.length);
+};
 export { getRandomNumber, getRandomIndex };
