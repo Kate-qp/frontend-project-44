c9138344b5814d7e28108ea43ffa6d3d7a0c48e8
Kate-qp <kpark4328@gmail.com>
2025-05-06 00:25:46 +0300

    Исправление файла

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,5 +1,5 @@
-import startGame from '../index.js';
-import { getRandomNumber } from '../../utils/random-value.js';
+import startGame from '../index.js'; // Убедитесь, что путь к index.js правильный
+import { getRandomNumber } from '../../utils/random-value.js'; // Убедитесь, что путь к random-value.js правильный
 const isPrime = (number) => {
   if (number < 2) {
     return false;
@@ -13,8 +13,9 @@ const isPrime = (number) => {
 };
 const description = 'Answer "yes" if the given number is prime. Otherwise answer "no".';
 const gameLogic = () => {
-  const randomNumber = getRandomNumber(1, 100);
-  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
-  return [`Question: ${randomNumber}`, correctAnswer];
+  const randomNumber = getRandomNumber(1, 100); // Генерируем случайное число от 1 до 100
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no'; // Определяем правильный ответ
+  return [`Question: ${randomNumber}`, correctAnswer]; // Возвращаем вопрос и правильный ответ в виде массива
 };
+// Запускаем игру
 export default () => startGame(description, gameLogic);
