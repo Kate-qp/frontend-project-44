5afb1c3f11a3ae92a9f651e42a3036d4fe823b05
Kate-qp <kpark4328@gmail.com>
2025-05-06 01:44:57 +0300

    Исправление файла

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,30 @@
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import globals from "globals";
-import stylistic from "@stylistic/eslint-plugin";
+import { defineConfig } from 'eslint/config'
+import js from '@eslint/js'
+import globals from 'globals'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { 
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
       js,
-      "@stylistic": stylistic 
-    }, 
-    extends: ["js/recommended"],
+      '@stylistic': stylistic
+    },
+    extends: ['js/recommended'],
     rules: {
-      "@stylistic/semi": ["error", "never"],
-      "@stylistic/quotes": ["error", "single"],
-      "@stylistic/eol-last": ["error", "always"],
-      "@stylistic/indent": ["error", 2],
-      "@stylistic/no-multi-spaces": ["error"],
-      "@stylistic/arrow-parens": ["error", "as-needed"]
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/no-multi-spaces': ['error'],
+      '@stylistic/arrow-parens': ['error', 'as-needed'],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }]
     }
   },
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    languageOptions: { 
-      globals: globals.browser 
-    } 
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser
+    }
   }
-]);
+])
