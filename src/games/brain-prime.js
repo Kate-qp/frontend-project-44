c50845bd94fe0433fc3f4bdd253d7f6a727c8dc5
Kate-qp <kpark4328@gmail.com>
2025-05-05 23:53:44 +0300

    Исправление файла brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,22 +1,21 @@
-import { getRandomNumber } from '../../utils/random-value.js';
 import startGame from '../index.js';
-const description =
-  'Answer "yes" if the given number is prime. Otherwise answer "no".';
-const isPrime = (num) => {
-  if (num <= 1) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
+import getRandomNumber from '../../utils/random-value.js';
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
   }
   return true;
 };
-const generateRound = () => {
-  const number = getRandomNumber() + 1;
+const DESCRIPTION = 'Answer "yes" if the given number is prime. Otherwise answer "no".';
+const getData = () => {
+  const number = getRandomNumber(1, 100);
   const question = `Question: ${number}`; 
   const correctAnswer = isPrime(number) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
-const startPrimeGame = () => {
-  console.log(description);
-  startGame(generateRound); 
-};
-export default startPrimeGame;
+export default () => startGame(DESCRIPTION, getData);
